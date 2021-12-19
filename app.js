ba69e6990accfa4eb9ae9c3d45f7df1cb928485e
c-cain <cjcain@protonmail.com>
2021-12-19 01:25:02 +0000

    .

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Matt Damon");
+var canvas = document.getElementById("stage");
+var ctx = canvas.getContext("2d");
+
+var x = 400;
+
+ctx.beginPath();
+    ctx.rect(x, 40, 50, 50);
+    ctx.fillStyle = '#FF0000';
+    ctx.fill();
+ctx.closePath();
+x++; 
